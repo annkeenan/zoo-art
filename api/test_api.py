3180202fe4cc2f4685ac3987feefcb122e7945c6
Taylor Murray <tmurray5@nd.edu>
2017-11-26 18:40:15 -0500

    post/put/delete tests

--- api/test_api.py
+++ api/test_api.py
@@ -63,6 +63,63 @@ class TestZooDatabase(unittest.TestCase):
         result = self.zdb.get_exhibit('Grizzly and Wolf Discovery Center')
         self.assertEquals(result, exp_result)
 
+    def test_put_species(self):
+        self.reset_database()
+        exp_result = {
+            'Common Name': set(['Wagler\'s Macaw']),
+            'Genus': 'fict',
+            'Family': 'fict1',
+            'Region': 'fict2',
+            'Habitat': set(['savanna/grassland', 'fict']),
+            'Status': 'LC'
+        }
+        self.zdb.load_species()
+        self.zdb.put_species('ara glaucogularis', exp_result)
+        result = self.zdb.get_species('ara glaucogularis')
+        self.assertEquals(result, exp_result)
+
+    def test_post_species(self):
+        self.reset_database()
+        post_dict = {
+            'Species': 'Dire wolf', 
+            'Common Name': set(['Dire wolf']),
+            'Genus': 'Canis',
+            'Family': 'Canidae',
+            'Region': 'Westoros',
+            'Habitat': set(['The North']),
+            'Status': 'EX'
+        }
+
+        exp_result = {
+            'Common Name': set(['Dire wolf']),
+            'Genus': 'Canis',
+            'Family': 'Canidae',
+            'Region': 'Westoros',
+            'Habitat': set(['The North']),
+            'Status': 'EX'
+        }
+        self.zdb.load_species()
+        self.zdb.post_species(post_dict)
+        result = self.zdb.get_species('Dire wolf')
+        self.assertEquals(result, exp_result)
+
+    def test_post_exhibit(self):
+        self.reset_database()
+        post_dict = {
+            'zoo name': 'Blah Zoo',
+            'species': set(['Kangaroo', 'Zebra'])
+        }
+        exp_result = set(['Kangaroo', 'Zebra']) 
+        self.zdb.load_exhibit()
+        self.zdb.post_exhibit(post_dict)
+        result = self.zdb.get_exhibit('Blah Zoo')
+        self.assertEquals(result, exp_result)
+
+    def test_delete_exhibit(self):
+        self.reset_database()
+        self.zdb.load_exhibit()
+        self.zdb.delete_exhibit('Birmingham Zoo')
+        self.assertEquals(self.zdb.get_exhibit('Birmingham Zoo'), None)
 
 if __name__ == "__main__":
     unittest.main()
