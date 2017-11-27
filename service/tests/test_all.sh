389d404618f2979c6afaacabd94064c939ca618c
Ann Keenan <akeenan2@nd.edu>
2017-11-27 15:48:48 -0500

    Initial test files

--- service/tests/test_all.sh
+++ service/tests/test_all.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
+printf "testing /classification/\n"
+python3 test_classification.py
+
+printf "testing /exhibit/\n"
+python3 test_exhibit.py
+
+printf "testing /habitat/\n"
+python3 test_.py
+
+printf "testing /region/\n"
+python3 test_.py
+
 printf "testing /reset/\n"
 python3 test_reset.py
+
+printf "testing /species/\n"
+python3 test_.py
+
+printf "testing /state/\n"
+python3 test_.py
+
+printf "testing /status/\n"
+python3 test_.py
+
+printf "testing /zoo/\n"
+python3 test_.py
