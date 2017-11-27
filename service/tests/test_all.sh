6b9d299b7ca8f0e6da22cc85968cd7348480fa03
Ann Keenan <akeenan2@nd.edu>
2017-11-27 15:49:20 -0500

    Fix all tests

--- service/tests/test_all.sh
+++ service/tests/test_all.sh
@@ -7,22 +7,22 @@ printf "testing /exhibit/\n"
 python3 test_exhibit.py
 
 printf "testing /habitat/\n"
-python3 test_.py
+python3 test_habitat.py
 
 printf "testing /region/\n"
-python3 test_.py
+python3 test_region.py
 
 printf "testing /reset/\n"
 python3 test_reset.py
 
 printf "testing /species/\n"
-python3 test_.py
+python3 test_species.py
 
 printf "testing /state/\n"
-python3 test_.py
+python3 test_state.py
 
 printf "testing /status/\n"
-python3 test_.py
+python3 test_status.py
 
 printf "testing /zoo/\n"
-python3 test_.py
+python3 test_zoo.py
