73eac15b248eee835a0828027e059828dbff06e3
Taylor Murray <tmurray5@nd.edu>
2017-11-30 00:24:58 -0500

    exhibit, state, species

--- webservice/main.py
+++ webservice/main.py
@@ -3,12 +3,12 @@ import context  # Add the parent directory to the path
 
 from ooapi import _zoo_database
 from classification import _classification_controller
-# from exhibit import _exhibit_controller
+from exhibit import _exhibit_controller
 from habitat import _habitat_controller
 from region import _region_controller
 from reset import _reset_controller
-# from species import _species_controller
-# from state import _state_controller
+from species import _species_controller
+from state import _state_controller
 # from status import _status_controller
 # from zoo import _zoo_controller
 
@@ -30,12 +30,12 @@ def start_service():
 ## Controllers
     options_controller = _options_controller()
     classification_controller = _classification_controller(zdb)
-    # exhibit_controller = _exhibit_controller(zdb)
+    exhibit_controller = _exhibit_controller(zdb)
     habitat_controller = _habitat_controller(zdb)
     region_controller = _region_controller(zdb)
     reset_controller = _reset_controller(zdb)
-    # species_controller = _species_controller(zdb)
-    # state_controller = _state_controller(zdb)
+    species_controller = _species_controller(zdb)
+    state_controller = _state_controller(zdb)
     # status_controller = _status_controller(zdb)
     # zoo_controller = _zoo_controller(zdb)
 
@@ -47,6 +47,9 @@ def start_service():
     dispatcher.connect(name='preflight_get_classification', route='/classification/:family', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
     dispatcher.connect(name='preflight_post_classification', route='/classification/:family', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
     # Exhibit
+    dispatcher.connect(name='preflight_get_exhibits', route='/exhibit/:zoo', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect(name='preflight_post_exhibit', route='/exhibit/:zoo', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect(name='preflight_delete_exhibit', route='/exhibit/:zoo', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
     # Habitat
     dispatcher.connect(name='preflight_get_habitats', route='/habitat/', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
     dispatcher.connect(name='preflight_get_habitat', route='/habitat/:habitat', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
@@ -58,7 +61,13 @@ def start_service():
     # Reset
     dispatcher.connect(name='preflight_reset_all', route='/reset/', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
     # Species
+    dispatcher.connect(name='preflight_get_all_species', route='/species/', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect(name='preflight_get_species', route='/species/:species', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect(name='preflight_put_species', route='/species/:species', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect(name='preflight_post_species', route='/species/', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
     # State
+    dispatcher.connect(name='preflight_get_states', route='/state/', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
+    dispatcher.connect(name='preflight_get_state', route='/state/:abbrev', controller=options_controller, action='OPTIONS', conditions=dict(method=['OPTIONS']))
     # Status
     # Zoo
 
@@ -68,6 +77,9 @@ def start_service():
     dispatcher.connect(name='get_classification', route='/classification/:family', controller=classification_controller, action='get_classification', conditions=dict(method=['GET']))
     dispatcher.connect(name='post_classification', route='/classification/', controller=classification_controller, action='post_classification', conditions=dict(method=['POST']))
     # Exhibit
+    dispatcher.connect(name='get_exhibits', route='/exhibit/:zoo', controller=exhibit_controller, action='get_exhibits', conditions=dict(method=['GET']))
+    dispatcher.connect(name='post_exhibits', route='/exhibit/:zoo', controller=exhibit_controller, action='post_exhibit', conditions=dict(method=['POST']))
+    dispatcher.connect(name='delete_exhibit', route='/exhibit/:zoo', controller=exhibit_controller, action='delete_exhibit', conditions=dict(method=['DELETE']))
     # Habitat
     dispatcher.connect(name='get_habitats', route='/habitat/', controller=habitat_controller, action='get_habitats', conditions=dict(method=['GET']))
     dispatcher.connect(name='get_habitat', route='/habitat/:habitat', controller=habitat_controller, action='get_habitat', conditions=dict(method=['GET']))
@@ -79,7 +91,13 @@ def start_service():
     # Reset
     dispatcher.connect(name='reset_all', route='/reset/', controller=reset_controller, action='reset_all', conditions=dict(method=['PUT']))
     # Species
+    dispatcher.connect(name='get_all_species', route='/species/', controller=species_controller, action='get_all_species', conditions=dict(method=['GET']))
+    dispatcher.connect(name='get_species', route='/species/:species', controller=species_controller, action='get_species', conditions=dict(method=['GET']))
+    dispatcher.connect(name='put_species', route='/species/:species', controller=species_controller, action='put_species', conditions=dict(method=['PUT']))
+    dispatcher.connect(name='post_species', route='/species/', controller=species_controller, action='post_species', conditions=dict(method=['POST']))
     # State
+    dispatcher.connect(name='get_states', route='/state/', controller=state_controller, action='get_states', conditions=dict(method=['GET']))
+    dispatcher.connect(name='get_state', route='/state/:abbrev', controller=state_controller, action='get_state', conditions=dict(method=['GET']))
     # Status
     # Zoo
 
@@ -87,7 +105,7 @@ def start_service():
     conf = {
         'global': {
             'server.socket_host': 'student04.cse.nd.edu',
-            'server.socket_port': 51042,
+            'server.socket_port': 51056,
         },
         '/': {
             'request.dispatch': dispatcher,
