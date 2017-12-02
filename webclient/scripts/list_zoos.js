a02ff5a3c72f2036bc144ab52866fc90d2e6f85b
Ann Keenan <akeenan2@nd.edu>
2017-12-02 15:21:01 -0500

    Add list zoo script

--- webclient/scripts/list_zoos.js
+++ webclient/scripts/list_zoos.js
@@ -4,6 +4,7 @@ function getZoos() {
   xmlHttp.onreadystatechange = function() {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
       response = JSON.parse(xmlHttp.response);
+      createLinks(response);
     }
   }
   xmlHttp.open("GET", "http://student04.cse.nd.edu:51042/zoo/", true);
@@ -12,11 +13,16 @@ function getZoos() {
 
 // Add links to the page
 function createLinks(data) {
-  links = '';
+  links = '<ul>';
   for (var z in data.zoos) {
     zoo_name = z.replace('_', ' ');
-    links += '<a href="/zoo.html?zoo=' + z + '">' + zoo_name + '</a>'
+    links += '<li><a href="/zoo.html?zoo=' + z + '">' + zoo_name + '</a></li>'
   }
+  links += '</ul>'
   html = $.parseHTML(links);
   $('#zoos').append(html);
 }
+
+$(document).ready(function() {
+  getZoos();
+});
