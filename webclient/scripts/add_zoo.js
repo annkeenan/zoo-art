69018e8aa5728d3d5f242a4a68bc5dcabcaa4094
Ann Keenan <akeenan2@nd.edu>
2017-12-02 21:34:11 -0500

    Fix chain of ajax calls

--- webclient/scripts/add_zoo.js
+++ webclient/scripts/add_zoo.js
@@ -1,43 +1,57 @@
-function getStates() {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      response = JSON.parse(xmlHttp.response);
-      createForm(response);
+function add_zoo() {
+  // Store ajax response values
+  var state_info;
+
+  // Start chain of ajax requests
+  getStates();
+
+  // Bind the form
+  $('#id_zoo_form').bind('submit', processForm);
+
+  function getStates() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        state_info = JSON.parse(xmlHttp.response);
+        createForm(response);
+      }
     }
+    xmlHttp.open("GET", "http://student04.cse.nd.edu:51042/state/", true);
+    xmlHttp.send(null);
   }
-  xmlHttp.open("GET", "http://student04.cse.nd.edu:51042/state/", true);
-  xmlHttp.send(null);
-}
 
-// Add links to the page
-function createDropdowns(states) {
-  // Hour
-  hours = '';
-  for (var i = 0; i < 24; i++) {
-    hours += '<option value="' + i + '">'+ i + '</option>'
+  // Add links to the page
+  function createDropdowns(states) {
+    // Hour
+    hours = '';
+    for (var i = 0; i < 24; i++) {
+      hours += '<option value="' + i + '">'+ i + '</option>'
+    }
+    $('#id_open_hour').append($.parseHTML(hours));
+    $('#id_close_hour').append($.parseHTML(hours));
+
+    // Minutes
+    minutes = '<option value="0">00</option><option value="30">30</option>';
+    $('#id_open_minute').append($.parseHTML(minutes));
+    $('#id_close_minute').append($.parseHTML(minutes));
+
+    // States
+    state_options = '';
+    for (var i in states.states) {
+      state_options += '<option value="' + states.states[i] + '">'+ states.states[i] + '</option>'
+    }
+    $('#id_state').append($.parseHTML(state_options));
   }
-  $('#id_open_hour').append($.parseHTML(hours));
-  $('#id_close_hour').append($.parseHTML(hours));
 
-  // Minutes
-  minutes = '<option value="0">00</option><option value="30">30</option>';
-  $('#id_open_minute').append($.parseHTML(minutes));
-  $('#id_close_minute').append($.parseHTML(minutes));
-
-  // States
-  console.log(states);
-  state_options = '';
-  for (var i in states.states) {
-    state_options += '<option value="' + states.states[i] + '">'+ states.states[i] + '</option>'
+  function createForm(states) {
+    createDropdowns(states);
   }
-  $('#id_state').append($.parseHTML(state_options));
-}
 
-function createForm(states) {
-  createDropdowns(states);
+  function processForm() {
+    console.log('processing');
+  }
 }
 
 $(document).ready(function() {
-  getStates();
+  add_zoo();
 });
--- webclient/scripts/edit_zoo.js
+++ webclient/scripts/edit_zoo.js
@@ -1,53 +1,71 @@
-// Get zoo
-function getZoo(zoo) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      zoo = JSON.parse(xmlHttp.response);
-      states = getStates();
-      createForm(zoo, states);
+function edit_zoo(zoo) {
+  // Store ajax response values
+  var zoo_info;
+  var state_info;
+
+  // Start chain of ajax requests
+  getZoo(zoo);
+
+  // Bind the form
+  $('#id_zoo_form').bind('submit', processForm);
+
+  // Get zoo
+  function getZoo(zoo) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        zoo_info = JSON.parse(xmlHttp.response);
+        getStates();
+      }
     }
+    xmlHttp.open("GET", "http://student04.cse.nd.edu:51042/zoo/" + zoo, true);
+    xmlHttp.send(null);
   }
-  xmlHttp.open("GET", "http://student04.cse.nd.edu:51042/zoo/" + zoo, true);
-  xmlHttp.send(null);
-}
 
-function getStates() {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      response = JSON.parse(xmlHttp.response);
-      return response;
+  // Get a list of states
+  function getStates() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        state_info = JSON.parse(xmlHttp.response);
+        createForm();
+      }
     }
+    xmlHttp.open("GET", "http://student04.cse.nd.edu:51042/state/", true);
+    xmlHttp.send(null);
   }
-  xmlHttp.open("GET", "http://student04.cse.nd.edu:51042/state/", true);
-  xmlHttp.send(null);
-}
 
-// Add links to the page
-function createDropdowns(states) {
-  // Hour
-  hours = '';
-  for (var i = 0; i < 24; i++) {
-    hours += '<option value="' + i + '">'+ i + '</option>'
-  }
-  $('#id_open_hour').append($.parseHTML(hours));
-  $('#id_close_hour').append($.parseHTML(hours));
-  // Minutes
-  minutes = ['00', '15', '30', '45'];
-  for (var m in minutes) {
-    console.log(m);
-  }
-  // States
-  console.log(states);
-}
+  // Add links to the page
+  function createDropdowns() {
+    // Hour
+    hours = '';
+    for (var i = 0; i < 24; i++) {
+      hours += '<option value="' + i + '">'+ i + '</option>'
+    }
+    $('#id_open_hour').append($.parseHTML(hours));
+    $('#id_close_hour').append($.parseHTML(hours));
 
-function createForm(zoo, states) {
-  createDropdowns(states);
+    // Minutes
+    minutes = '<option value="0">00</option><option value="30">30</option>';
+    $('#id_open_minute').append($.parseHTML(minutes));
+    $('#id_close_minute').append($.parseHTML(minutes));
+
+    // States
+    state_options = '';
+    for (var i in state_info.states) {
+      state_options += '<option value="' + state_info.states[i] + '">'+ state_info.states[i] + '</option>'
+    }
+    $('#id_state').append($.parseHTML(state_options));
+
+  }
+
+  function createForm() {
+    createDropdowns();
+  }
 }
 
 $(document).ready(function() {
   let params = (new URL(document.location)).searchParams;
   let zoo = params.get("zoo");
-  getZoo(zoo);
+  edit_zoo(zoo);
 });
